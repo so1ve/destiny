106213f60db6f0ad9d1154febf042df3d64e0098
ʀᴀʏ <nn_201312@163.com>
2022-06-08 17:36:28 +0800

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "so1ve/destiny:main"
+    [String]$upstream = "so1ve/destiny:master"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
