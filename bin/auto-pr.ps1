5a330052ef35b4706c90737c6e8f2b02e8fa03c8
so1ve <nn_201312@163.com>
2022-06-27 14:48:51 +0800

    update

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -4,6 +4,6 @@ param(
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-$autopr = "./bin/bin__auto-pr.ps1" # Invoked by the auto-pr action, adds 'bin' folder
+$autopr = "./bin__auto-pr.ps1"
 $dir = "$PSScriptRoot/../bucket" # checks the parent dir
 Invoke-Expression -command "& '$autopr' -dir '$dir' -upstream $upstream $($args | ForEach-Object { "$_ " })"
