7ccdca1bffed292d2fb7ee19e5e4f369625edc99
so1ve <nn_201312@163.com>
2022-06-27 14:26:05 +0800

    feat: stash every version

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -3,7 +3,7 @@ param(
     [String]$upstream = "so1ve/destiny:master"
 )
 
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-$autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
+if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
+$autopr = "./auto-pr.ps1"
 $dir = "$PSScriptRoot/../bucket" # checks the parent dir
 Invoke-Expression -command "& '$autopr' -dir '$dir' -upstream $upstream $($args | ForEach-Object { "$_ " })"
