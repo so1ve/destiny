8717528f70c5de7a5b6f96f731ecc067823faa24
so1ve <nn_201312@163.com>
2022-06-27 14:31:45 +0800

    fix

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -4,6 +4,6 @@ param(
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-$autopr = "./auto-pr.ps1"
+$autopr = "./bin__auto-pr.ps1"
 $dir = "$PSScriptRoot/../bucket" # checks the parent dir
 Invoke-Expression -command "& '$autopr' -dir '$dir' -upstream $upstream $($args | ForEach-Object { "$_ " })"
